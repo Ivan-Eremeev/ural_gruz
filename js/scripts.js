1ed7ac7481a099cf7e521fb5974ca1f1dcba0f63
Ivan <ivan.frontcoder@gmail.com>
2022-05-17 18:41:00 +0500

    .

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+
+  // Swiper slider
+  const welcomeSlider = new Swiper('#welcomeSlider', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+}
