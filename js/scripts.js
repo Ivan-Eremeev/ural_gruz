66e8ba348ef2351203c199ab630fd86da5537bb0
Ivan <ivan.frontcoder@gmail.com>
2022-05-31 18:09:36 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -305,4 +305,36 @@ window.onload = () => {
   // JQuery Scrollbar
   $('.scrollbar-inner').scrollbar();
 
+  // Изменение количества товара (плюс минус)
+  function counter(block) {
+    const counter = document.querySelectorAll(block);
+    if (counter) {
+      counter.forEach(element => {
+        const minus = element.querySelector('.js-counter-minus');
+        const plus = element.querySelector('.js-counter-plus');
+        const inputWrap = element.querySelector('.js-counter-input');
+        const input = inputWrap.querySelector('input');
+        plus.addEventListener('click', () => {
+          if (Number(input.value) < 999) {
+            input.value = Number(input.value) + 1;
+          }
+        })
+        minus.addEventListener('click', () => {
+          if (Number(input.value) > 1) {
+            input.value = Number(input.value) - 1;
+          }
+        })
+        input.addEventListener('keyup', () => {
+          input.value = input.value.replace(/[^\d]/g, '');
+        })
+        input.addEventListener('blur', () => {
+          if (input.value == '' || input.value == 0) {
+            input.value = 1;
+          }
+        })
+      });
+    }
+  }
+  counter('.js-counter');
+
 }
